479f3c8457f5cee21318abdaf78acd6b4c08556d
George Fairbairn <george.fair@icloud.com>
2018-08-07 18:38:18 +0000

    Player enters name and is now stored in the players.txt file

--- run.py
+++ run.py
@@ -1,68 +1,27 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
-# def show_riddle():
-    
-#     riddles = []
-#     solutions = []
-    
-#     with open('riddle.txt', "r") as file:
-#         lines = file.read().splitlines()
-        
-#     for i, text in enumerate(lines):
-#         if i%2 == 0:
-#             riddles.append(text)
-#         else:
-#             solutions.append(text)
-            
-#     number_of_riddles = len(riddles)
-#     riddles_and_solutions = zip(riddles, solutions)
-    
-#     score = 0
-    
-#     for riddle, solution in riddles_and_solutions:
-#         guess = input(riddle +"> ")
-#         if guess == solution:
-#             score +=1
-#             print("Correct answer!")
-#         else:
-#             print("Wrong answer :(")
-            
-def get_riddle():
-    
-    riddles = []
-    
-    with open("data/riddle.txt", "r") as file:
-        lines = file.read().splitlines()
-        
-    for i, text in enumerate(lines):
-            if i%2 == 0:
-                riddles.append(text)
-    
-    return riddles
-    
-def get_solutions():
-    
-    solutions = []
-    
-    with open("data/riddle.txt", "r") as file:
-        lines = file.read().splitlines()
-        
-    for i, text in enumerate(lines):
-        if i%2 != 0:
-            solutions.append(text)
-        
-    return solutions
-    
+data = []
 
-@app.route('/')
+def write_to_file(filename, data):
+    """ Handle the process of writing data to a file """
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+@app.route('/', methods = ["GET", "POST"])
 def index():
-    """ Main Riddle Game Landing Page """
+    """ Main landing page """
     
-    riddles = get_riddle()
-    solutions = get_solutions()
-    return render_template('index.html', riddles = riddles, solutions = solutions)
+    if request.method == "POST":
+        write_to_file("data/players.txt", request.form["player_name"] + "\n")
+        return redirect(request.form["player_name"])
+    return render_template("index.html")
     
-app.run(host = os.getenv('IP'), port = int(os.getenv('PORT')), debug = True)
+@app.route('/<player_name>')
+def riddles(player_name):
+    """ Game page """
+    return render_template("riddles_game.html")
+
+app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)), debug = True)
